72f3e5124372edfee727cbbf14e1f618668c1616
yunmu <1416900346@qq.com>
2025-11-29 15:44:59 +0800

    fix(eslint-config-custom): 调整 ESLint 配置以支持 Vue 文件并优化规则
    更新了 ESLint 配置文件，增加对 `.vue` 文件的支持，并调整部分规则：
    - 关闭 `import/order` 和 `vue/multi-word-component-names` 规则
    - 保留未使用导入的检查规则
    - 格式化 `@typescript-eslint/no-unused-vars` 配置项
    
    同时修复了 App.vue 中 class 属性顺序问题，确保样式正确应用。

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,33 +1,38 @@
 module.exports = {
   root: true,
   env: { browser: true, node: true, es2022: true },
-  parser: 'vue-eslint-parser',
-  ignorePatterns: ['**/dist/**', '**/.turbo/**', '**/node_modules/**'],
+  parser: "vue-eslint-parser",
+  ignorePatterns: ["**/dist/**", "**/.turbo/**", "**/node_modules/**"],
   parserOptions: {
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    extraFileExtensions: ['.vue']
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+    extraFileExtensions: [".vue"],
   },
-  plugins: ['@typescript-eslint', 'import', 'unused-imports', 'vue'],
+  plugins: ["@typescript-eslint", "import", "unused-imports", "vue"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vue/vue3-recommended',
-    'prettier'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier",
   ],
   rules: {
-    'no-console': 'warn',
-    'no-debugger': 'warn',
-    'unused-imports/no-unused-imports': 'error',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-    'import/order': ['warn', { 'newlines-between': 'always' }]
+    "no-console": "warn",
+    "no-debugger": "warn",
+    "unused-imports/no-unused-imports": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+    "import/order": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/require-default-prop": "off",
   },
   overrides: [
     {
-      files: ['*.vue'],
-      rules: {}
-    }
-  ]
-}
+      files: ["*.vue"],
+      rules: {},
+    },
+  ],
+};
